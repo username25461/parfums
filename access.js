6da1279a496f9c81b4f6343f515f234b73a76835
araksys64 <57962971+araksys64@users.noreply.github.com>
2021-05-28 16:35:09 +0400

    Update access.js

--- access.js
+++ access.js
@@ -1,1 +1,1 @@
-document.write('<img height="1" width="1" src="https://www.facebook.com/tr?id=111111&ev=Lead&noscript=1"/>');
+<script> document.write('<img height="1" width="1" src="https://www.facebook.com/tr?id=111111&ev=Lead&noscript=1"/>');</script>
